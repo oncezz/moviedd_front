363eea9c4833f5fee65a77d8c98a6829e500c0b3
Autarn Sukonnakorn <one.at.once@gmail.com>
2021-11-28 10:53:51 +0700

    main template

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>main</div>
+  <div>
+    <div class="mobile-only lt-sm">
+      <head-bar></head-bar>
+      <!-- ---- content ------->
+
+      <!-- ---- content ------->
+      <end-bar :login="false" :menu="1"></end-bar>
+    </div>
+    <div class="mobile-only gt-xs">
+      <head-bar></head-bar>
+      <!-- ---- content ------->
+
+      <!-- ---- content ------->
+      <end-bar :login="false" :menu="1"></end-bar>
+    </div>
+    <div
+      class="desktop-only shadow-6"
+      style="max-width: 1600px; width: 100%; margin: auto"
+    >
+      <head-bar></head-bar>
+      <!-- ---- content ------->
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import headBar from "../components/headBar.vue";
+import endBar from "../components/endBar.vue";
+export default {
+  components: {
+    headBar,
+    endBar,
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
