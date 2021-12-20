290e0a55c1bfc6c23de23a5a14aff1a7903a39f1
Ohm <destiny30094@gmail.com>
2021-12-20 16:01:43 +0700

    moviecat

---
+++ src/pages/moviecategory.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <!-- pc  -->
+    <div
+      class="desktop-only shadow-6"
+      style="max-width: 1600px; width: 100%; margin: auto"
+    >
+      <div class="backGroundPc">
+        <div class="topTextPc q-pt-md" align="center">ประเภทหนัง</div>
+
+        <div class="categoryNamePc">
+          <div
+            v-for="(item, index) in category"
+            :key="index"
+            class="q-pa-md"
+            align="center"
+          >
+            <div class="categoryTextPc">
+              {{ item.catname }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      category: [],
+    };
+  },
+  methods: {
+    async loadCategory() {
+      let url = this.serverpath + "fe_category_loadcategory.php";
+      let res = await axios.get(url);
+
+      this.category = res.data;
+      this.category.sort((a, b) => a.orderid - b.orderid);
+    },
+  },
+  mounted() {
+    this.loadCategory();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.backGroundPc {
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(180deg, #00d1ff -47.95%, #000000 78.61%);
+}
+.headTopPc {
+  width: 100%;
+  height: 82px;
+  background: #000000;
+}
+.topTextPc {
+  font-size: 36px;
+  font-weight: bold;
+  color: #00d1ff;
+}
+.categoryTextPc {
+  display: inline-block;
+  cursor: pointer;
+  font-size: 34px;
+  color: white;
+}
+.categoryTextPc:hover {
+  color: yellow;
+}
+.categoryNamePc {
+  overflow-y: auto;
+  height: calc(100vh - 100px);
+}
+</style>
---
+++ src/pages/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="boxRed">
+      123 <br />
+      456 <br />
+      789 <br />
+      123 <br />
+      456 <br />
+      789 <br />
+      123 <br />
+      456 <br />
+      789 <br />
+      123 <br />
+      456 <br />
+      789 <br />
+      adfjahd
+      adfjahdfgajhglakjfhaldjfgnakfjhna;ldkjghafkjhna;ldfkhgnakjfgna;lhgnakngakjfhlna;ldfkjghnak
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.boxRed {
+  overflow-x: auto;
+  overflow-y: scroll;
+  width: 300px;
+  height: 300px;
+  background: red;
+}
+</style>
